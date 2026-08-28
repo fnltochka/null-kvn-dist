7f39ad3a80ebe5e1be03d011cb0219a1c690dc51
FnlTochka <me@fnltochka.ru>
2026-08-28 09:44:00 +0000

    fix(release): bind rc2 product identity

--- release.nix
+++ release.nix
@@ -1,7 +1,7 @@
 {
   releaseId = "20260828T090846Z-68b3ef0c";
   version = "2026.8.0-rc.2";
-  candidateSha = "9bcad98c5b5c4f22b1350f7c46ba9639e3469e34";
+  candidateSha = "e426419f2709d08c815f63eefef68e53d909dd1b";
   authorityPublicKeyHex = "7240d3d5a8abf18e3c2559b26198d4db822060fd88615b21a1823d3c94ce0b4d";
   baseUrl = "https://releases.null-kvn.fnltochka.dev/v1/releases";
 
