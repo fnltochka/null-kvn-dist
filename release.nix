34ebc593a11a441cbd2f673dd12ce7551032ee48
FnlTochka <me@fnltochka.ru>
2026-08-31 03:08:40 +0000

    chore(release): publish 2026.8.0-rc.13 client

--- release.nix
+++ release.nix
@@ -1,11 +1,11 @@
 {
-  releaseId = "20260831T001856Z-739e35af";
-  version = "2026.8.0-rc.11";
-  candidateSha = "1c22f6472e3ecbf1753904bfc02cf5e5dacd4ffe";
+  releaseId = "20260831T024955Z-f0a5812e";
+  version = "2026.8.0-rc.13";
+  candidateSha = "4b54680764853d304c0c06b366b9076c7f8496fb";
   authorityPublicKeyHex = "7240d3d5a8abf18e3c2559b26198d4db822060fd88615b21a1823d3c94ce0b4d";
   baseUrl = "https://releases.null-kvn.fnltochka.dev/v1/releases";
 
-  manifestSha256 = "sha256-ubTEjOT1EkfF9aShRAhX5AMMUEHLeO7DwO/ZJmlHLzI=";
-  signatureSha256 = "sha256-ho0Tbq2xcPolcQCxxezLKHhzcl8RV6rQ/yg3VRpvoO0=";
-  artifactSha256 = "sha256-ZVfJg+L9aPxjYXy7/p58VYl1eAG3Nzhz7MwG4uNJRRs=";
+  manifestSha256 = "sha256-CmesLYJocvWWd2aKPhRw5LwudjrEBq9+AV4rMz7kDbw=";
+  signatureSha256 = "sha256-Y4RsDwj/D/a6+CpFUgn8aN4SWFVlsp8fNKBEn7e9RFM=";
+  artifactSha256 = "sha256-c1rW2rAxnErtvJSERDltp9sIpNz2Bj4e4Tq2FerLp/4=";
 }
